94b499a21971b98f9090d5ec6aaee620c7433008
Nikita Sukharevich <sukharevichn@gmail.com>
2025-03-31 18:12:09 -0400

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/portfolio',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
